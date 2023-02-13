b8213a3402ecd446d0ad35df29a47d374a497c71
zugf <florian.zug@zeppelin.com>
2023-02-13 12:08:44 +0000

    add slim and appinspect to build script

--- build.sh
+++ build.sh
@@ -2,3 +2,13 @@
 python -m pip install -r nl-processor/requirements.txt -t nl-processor/lib
 python -m pip install -r nl-processor/requirements-dev.txt
 python -m markdown2 -x target-blank-links DOCS.md > nl-processor/appserver/static/docs.html
+python -m slim package nl-processor -o deploy -u error
+# run appinspect... needs to be in a venv because of dependency conflicts
+# also needs libmagic installed (if not installed, execute the next two lines)
+# sudo apt-get update -y
+# sudo apt-get install -y libmagic-dev
+python -m venv .appinspect
+. ./.appinspect/bin/activate
+pip install splunk-appinspect
+splunk-appinspect inspect ./deploy/nl-processor-*.tar.gz
+deactivate
