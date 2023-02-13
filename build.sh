4f2ca3e748217bddc27ea803964bf38f003bc3dc
zugf <florian.zug@zeppelin.com>
2023-02-13 13:43:34 +0000

    use normal install for slim
    still will show error, but installation (and tool) is working

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ python -m pip install -r nl-processor/requirements.txt -t nl-processor/lib
 python -m pip install -r nl-processor/requirements-dev.txt
 python -m markdown2 -x target-blank-links DOCS.md > nl-processor/appserver/static/docs.html
 
-# slim (installation needs to be --user otherwise permission errors since its setup will try to write to /usr/share)
-python -m pip install --user splunk-packaging-toolkit==1.0.1 semantic_version==2.6.0
+# slim (installation will show a permission error, but does work nevertheless - man link is not needed)
+python -m pip install splunk-packaging-toolkit==1.0.1 semantic_version==2.6.0
 python -m slim package nl-processor -o deploy -u error
 
 # run appinspect... needs to be in a venv because of dependency conflicts
@@ -14,5 +14,5 @@ python -m slim package nl-processor -o deploy -u error
 python -m venv .appinspect
 . ./.appinspect/bin/activate
 pip install splunk-appinspect
-splunk-appinspect inspect ./deploy/nl-processor-*.tar.gz
+splunk-appinspect inspect ./deploy/nl-processor-*.tar.gz --mode test --generate-feedback  --output-file "deploy/AppInspect-results.json"
 deactivate
