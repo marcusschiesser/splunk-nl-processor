6bcb1cf79f6c558ba34e83cb4a5edeca2bf8b487
Marcus Schiesser <mail@marcusschiesser.de>
2023-02-14 07:50:01 +0000

    remove venv for appinspect

--- build.sh
+++ build.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
+# install app's dependencies
 python -m pip install -r nl-processor/requirements.txt -t nl-processor/lib
-python -m pip install -r nl-processor/requirements-dev.txt
-python -m markdown2 -x target-blank-links DOCS.md > nl-processor/appserver/static/docs.html
+chmod 644 nl-processor/lib/charset_normalizer/*.so # otherwise app-inspect fails
 
-# slim (installation will show a permission error, but does work nevertheless - man link is not needed)
-python -m pip install splunk-packaging-toolkit==1.0.1 semantic_version==2.6.0
+# install build dependencies
+python -m pip install -r nl-processor/requirements-dev.txt
+
+# build docs
+python -m markdown2 -x target-blank-links nl-processor/README > nl-processor/appserver/static/docs.html
+
+# build package
 python -m slim package nl-processor -o deploy -u error
 
-# run appinspect... needs to be in a venv because of dependency conflicts
-# also needs libmagic installed (if not installed, execute the next two lines)
-# sudo apt-get update -y
-# sudo apt-get install -y libmagic-dev
-python -m venv .appinspect
-. ./.appinspect/bin/activate
-pip install splunk-appinspect
+# check package
 splunk-appinspect inspect ./deploy/nl-processor-*.tar.gz --mode test --generate-feedback  --output-file "deploy/AppInspect-results.json"
-deactivate
