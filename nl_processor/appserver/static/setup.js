1843ac48ea33f7681e8f830302c980d599535b89
zugf <florian.zug@zeppelin.com>
2023-11-08 17:43:29 +0100

    add azure_support

--- nl_processor/appserver/static/setup.js
+++ nl_processor/appserver/static/setup.js
@@ -6,6 +6,9 @@ require(["jquery", "splunkjs/splunk", "splunkjs/ready!"], function (
 ) {
   const apiTokenInput = $("splunk-text-input[name='hf_api_token']");
   const openAIapiKeyInput = $("splunk-text-input[name='openai_api_key']");
+  const azureResourceNameInput = $(
+    "splunk-text-input[name='azure_resource_name']"
+  );
 
   const http = new splunkjs.SplunkWebHttp();
   const service = new splunkjs.Service(http, {
@@ -13,14 +16,15 @@ require(["jquery", "splunkjs/splunk", "splunkjs/ready!"], function (
     app: appName,
     sharing: "app",
   });
+  const endpoint = new splunkjs.Service.Endpoint(service, "configure");
 
   const configure = async function () {
     return new Promise(function (resolve, reject) {
       const options = {
         hf_api_token: apiTokenInput.attr("value") || "",
         openai_api_key: openAIapiKeyInput.attr("value") || "",
+        azure_resource_name: azureResourceNameInput.attr("value") || "",
       };
-      const endpoint = new splunkjs.Service.Endpoint(service, "configure");
       endpoint.post("", options, function (err, response) {
         if (err) {
           console.log(err);
@@ -43,4 +47,17 @@ require(["jquery", "splunkjs/splunk", "splunkjs/ready!"], function (
   };
 
   $("#setupButton").click(performSetup);
+
+
+  endpoint
+    .get()
+    .then((response) => {
+      const jsonData = JSON.parse(response);
+      $("#azure_resource_name").attr("value", jsonData["azure_resource_name"]);
+      $("#hf_api_token").attr("value", jsonData["hf_api_token"]);
+      $("#openai_api_key").attr("value", jsonData["openai_api_key"]);
+    })
+    .catch((error) => {
+      alert("Error reading configuration: " + error.error);
+    });
 });
--- nl_processor/bin/handlers/configure.py
+++ nl_processor/bin/handlers/configure.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 
 from splunklib import client
 from splunk.persistconn.application import PersistentServerConnectionApplication
-from passwords import encode_password
+from passwords import encode_password, decode_password
 
 
 def flatten_query_params(params):
@@ -17,6 +17,10 @@ def flatten_query_params(params):
     return flattened
 
 
+def is_unchanged_password(text):
+    return all(char == "*" for char in text)
+
+
 class ConfigureHandler(PersistentServerConnectionApplication):
     def __init__(self, _command_line, _command_arg):
         PersistentServerConnectionApplication.__init__(self)
@@ -24,22 +28,15 @@ class ConfigureHandler(PersistentServerConnectionApplication):
     def handle(self, in_string):
         try:
             request = json.loads(in_string)
-            payload = flatten_query_params(request["form"])
-            hf_api_token = payload["hf_api_token"]
-            openai_api_key = payload["openai_api_key"]
-
             service = client.Service(
                 token=request["system_authtoken"], sharing="app", app="nl_processor"
             )
-            encode_password(service, "hf_api_token", hf_api_token)
-            encode_password(service, "openai_api_key", openai_api_key)
 
-            return {"payload": {"success": "true"}, "status": 200}
-        except (KeyError, ValueError) as e:
-            return {
-                "payload": {"success": "false", "result": f"Error parsing JSON: {e}"},
-                "status": 400,
-            }
+            if request["method"] == "POST":
+                payload = flatten_query_params(request["form"])
+                return self.handle_post(service, payload)
+            else:
+                return self.handle_get(service)
         except Exception as ex:
             import traceback
 
@@ -50,4 +47,46 @@ class ConfigureHandler(PersistentServerConnectionApplication):
                     "traceback": traceback.format_exc(),
                 },
                 "status": 500,
+            }
+
+    def handle_get(self, service):
+        conf = service.confs["commands"]
+        stanza = conf["openai"]
+
+        azure_resource_name = (
+            stanza["azure_resource_name"] if "azure_resource_name" in stanza else ""
+        )
+        hf_api_token = "***" if decode_password(service, "hf_api_token") else ""
+        openai_api_key = "***" if decode_password(service, "openai_api_key") else ""
+
+        return {
+            "payload": {
+                "success": "true",
+                "azure_resource_name": azure_resource_name,
+                "hf_api_token": hf_api_token,
+                "openai_api_key": openai_api_key,
+            },
+            "status": 200,
+        }
+
+    def handle_post(self, service, payload):
+        try:
+            hf_api_token = payload["hf_api_token"]
+            openai_api_key = payload["openai_api_key"]
+            azure_resource_name = payload["azure_resource_name"]
+
+            conf = service.confs["commands"]
+            stanza = conf["openai"]
+            stanza.submit({"azure_resource_name": azure_resource_name})
+
+            if not is_unchanged_password(hf_api_token):
+                encode_password(service, "hf_api_token", hf_api_token)
+            if not is_unchanged_password(openai_api_key):
+                encode_password(service, "openai_api_key", openai_api_key)
+
+            return {"payload": {"success": "true"}, "status": 200}
+        except (KeyError, ValueError) as e:
+            return {
+                "payload": {"success": "false", "result": f"Error parsing JSON: {e}"},
+                "status": 400,
             }
